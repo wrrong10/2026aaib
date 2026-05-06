c630d2b15f659129db8a4175f276b2fa8df6c6fd
wrrong10 <14750375@me.mcu.edu.tw>
2026-05-06 08:50:39 +0800

    week06

--- week06/week06-3b.cpp
+++
@@ -1,8 +0,0 @@
-// week06-3b.cpp SOIT_BASE_007
-#include <stdio.h>
-int main()
-{
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d\n", a/b);
-}
--- week06/week06-3c.cpp
+++
@@ -1,8 +0,0 @@
-// week06-3c.cpp SOIT_BASE_003
-#include <stdio.h>
-int main()
-{
-    int a, b;
-    scanf("%d%d", &a, &b);
-    printf("%d", a%b );
-}
